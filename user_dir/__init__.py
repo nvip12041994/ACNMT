5cf2cdb3a04ba8ec1a4abca6ea149710c2198519
nvip12041994 <nvip12041994@gmail.com>
2022-07-01 00:47:58 +0900

    Add script preprocessing and some fix for train wmt14

--- user_dir/__init__.py
+++ user_dir/__init__.py
@@ -2,3 +2,4 @@ from . import RL
 from . import modify_label_smoothed_cross_entropy
 from . import pg_valid_cross_entropy
 from . import pg_cross_entropy
+from . import mod_label_smoothed_cross_entropy
